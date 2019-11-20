5ef58306d0e6308d5a7aa4c1133011861f9f7479
FoxFireShot <44412556+FoxFireShot@users.noreply.github.com>
2019-11-20 08:29:45 -0300

    Add files via upload
    CRC - função crcFast recebe uma struct e retorna o seu crc

---
+++ main.c
@@ -0,0 +1,39 @@
+/**********************************************************************
+ *
+ * Filename:    main.c
+ * 
+ * Description: A simple test program for the CRC implementations.
+ *
+ * Notes:       To test a different CRC standard, modify crc.h.
+ *
+ * 
+ * Copyright (c) 2000 by Michael Barr.  This software is placed into
+ * the public domain and may be used for any purpose.  However, this
+ * notice must not be changed or removed and no warranty is either
+ * expressed or implied by its publication or distribution.
+ **********************************************************************/
+
+#include "crc.h"
+
+void main(void)
+{
+	unsigned char  test[] = "123456789";
+	struct teste a;
+
+	sprintf(a.dado,"12345678999990909");
+	/*
+	 * Print the check value for the selected CRC algorithm.
+	 */
+	printf("The check value for the %s standard is 0x%X\n", CRC_NAME, CHECK_VALUE);
+	
+	/*
+	 * Compute the CRC of the test message, slowly.
+	 */
+	printf("The crcSlow() of %s is 0x%X\n", test, crcSlow(test, strlen(test)));
+	
+	/*
+	 * Compute the CRC of the test message, more efficiently.
+	 */
+	crcInit();
+	printf("The crcFast() of %s is 0x%X\n", test, crcFast(&a));
+}
